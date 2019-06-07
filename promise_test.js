29f332de99f5f988dd8ca8c15d68dfd409a31b91
Bastien Mercier <bastien.mercier@capgemini.com>
2019-06-07 10:09:41 +0200

    test promise

--- promise_test.js
+++ promise_test.js
@@ -1,54 +1,21 @@
-Promise.all([1,2,3,4].map(v => {
+const get_country = () => {
     return new Promise((resolve, reject) => {
-        if (v === 3) {
-            reject('something went wrong')
-        } else {
-            resolve(v)
-        }
+        resolve({
+            code: 'FR', 
+            value: 'France'
+        })
     })
-}))
-.then(value => {
-    console.log(value)
+}
+
+
+const promises = [1,2,3].map(val => {
+    return get_country(val)
+        .then(country => {
+            console.log(country)
+            return Promise.resolve(country.value)
+        })
 })
-.catch(err => {
-    console.log(err)
-})
 
-
-
-
-Promise.resolve('success -1')
-    .then(() => Promise.reject('error 0'))
-    .then(() => Promise.resolve('success 2'))
-    .catch(err => console.log(err))
-
-
-const first$ = () => {
-    console.log('execute function first')
-    return Promise.resolve('first')
-}
-const second$ = () => {
-    console.log('execute function second')
-    return Promise.resolve('first')
-}
-const error$ = () => {
-    console.log('execute function error')
-    return Promise.resolve('error')
-}
-
-Promise.all([null, null, null])
-    .then(() => console.log('success!!'))
-    .catch(err => console.log(err))
-
-
-// Promise.resolve('hello')
-// .then(res => console.log(res))
-// .then(() => {
-//     console.log('end')
-// })
-// .then(() => {
-//     console.log('after the end')
-// })
-
-
+Promise.all(promises)
+    .then(res => console.log(res))
 
